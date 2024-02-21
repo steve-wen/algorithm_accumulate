f922cf781b08b34547926e3b1c749f90d1357d19
wenjunjie <1120503418@qq.com>
2024-02-21 18:14:22 +0800

    z algorithm + trie

--- src/main.cpp
+++ src/main.cpp
@@ -6358,6 +6358,81 @@ int numberOfPairs(vector<vector<int>>& p) {
     return ans;
 }
 
+vector<int> z_function(string s) {
+    int n = (int)s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r && z[i - l] < r - i + 1) {
+            z[i] = z[i - l];
+        } else {
+            z[i] = max(0, r - i + 1);
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) ++z[i];
+        }
+        if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+    }
+    return z;
+}
+
+class Node1 {
+public:
+    array<Node1*, 26> children{};
+    int cnt = 0;
+};
+
+class Trie1 {
+public:
+    Node1 *root = new Node1();
+
+    void insert(string w) {
+        auto z = z_function(w);
+        int n = w.size();
+        z[0] = n;
+        Node1 *cur = root;
+        for (int i = 0; i < n; ++i) {
+            auto c = w[i];
+            if (cur->children[c-'a'] == nullptr) {
+                cur->children[c-'a'] = new Node1();
+            }
+            cur = cur->children[c-'a'];
+            if (z[n-i-1] == i+1) {
+                cur->cnt++;
+            }
+        }
+    }
+
+    int query(string p) { // dfs
+        Node1 *cur = root;
+        int tmp = 0;
+        for (int i = 0; i < p.size(); ++i) {
+            auto c = p[i];
+            if (cur->children[c-'a'] == nullptr) {
+                return tmp;
+            }
+            cur = cur->children[c-'a'];
+            if (i == p.size()-1) {
+                return cur->cnt;
+            }
+        }
+        return tmp;
+    }
+};
+
+class Solution {
+public:
+    long long countPrefixSuffixPairs(vector<string>& w) {
+        Trie1 t{};
+        long long ans = 0;
+        int n = w.size();
+        for (int i = n-1; i >= 0; --i) {
+            ans += (long long)t.query(w[i]);
+            t.insert(w[i]);
+        }
+        return ans;
+    }
+};
+
+
+
 int main() {
 
     return 0;
@@ -6503,7 +6578,6 @@ int main() {
  * 7.6 贡献法
  * 7.8 巫师的力量总和
  * 8. 莫队算法
- * 1. 370 周赛第 3，4 题
  * 4. 2200 难度题
  * 5. 灵神 总结/归纳 的周赛题单（附难度分和知识点）-> 对应练习
  * 6. no.887 鸡蛋掉落
